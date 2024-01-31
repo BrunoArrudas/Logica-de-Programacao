d2e07766d334a485896fcd1d67c0f7f5f3e051ff
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2024-01-30 21:29:38 -0300

    Atualização 0.10

--- aula10/script.js
+++ aula10/script.js
@@ -24,8 +24,13 @@ Exercício:
     - A recuperação de via não pode fazer o personaem ir além da vida máxima
 
 Parte 3: Crie a herança
-    - Crie as classes Arqueir, Guerreiro e Mago
+    - Crie as classes Arqueiro, Guerreiro e Mago
     - Todas as 3 classes herdam de personagem
+
+Alterações do Guerreiro:
+    - Acrescente a propriedade "escudo" na classe Guerreiro.
+    - A função tomarDano do Guerreiro deve proteger seus pontos de vida,
+    abatendo o dano sofrido dos pontos do seu escudo.
 */
 
 class Personagem {
@@ -45,7 +50,7 @@ class Personagem {
     }
 
     tomarDano(quantidade) {
-        if (this.vivo) {            
+        if (this.vivo) {
             this.vida = this.vida - quantidade;
             console.log(`${this.nome} sofreu ${quantidade} de dano, e agora tem ${this.vida} de vida`)
 
@@ -59,7 +64,7 @@ class Personagem {
     }
 
     atacar(inimigo) {
-        if(inimigo.vivo && this.vivo) {
+        if (inimigo.vivo && this.vivo) {
             console.log(`${this.nome} atacou ${inimigo.nome} com força de ${this.ataque}`);
             inimigo.tomarDano(this.ataque);
         } else {
@@ -67,10 +72,10 @@ class Personagem {
         }
     }
 
-    recuperarVida(vidaRecebida, personagem = this){
-        personagem.vida += vidaRecebida;        
+    recuperarVida(vidaRecebida, personagem = this) {
+        personagem.vida += vidaRecebida;
 
-        if(personagem.vida > personagem.vidaMaxima) {
+        if (personagem.vida > personagem.vidaMaxima) {
             personagem.vida = personagem.vidaMaxima;
         }
 
@@ -85,8 +90,19 @@ class Arqueiro extends Personagem {
 }
 
 class Guerreiro extends Personagem {
-    constructor(nome, ataque, defesa, vida, posicao, vivo = true) {
+    constructor(nome, ataque, defesa, vida, posicao, vivo = true, escudo) {
         super(nome, ataque, defesa, vida, posicao, vivo)
+        this.escudo = escudo;
+    }
+
+    tomarDano(quantidade) {
+        console.log(`${this.nome} sofre dano de ${quantidade}, mas defendeu com ${this.escudo} de escudo`);
+        if (quantidade > this.escudo) {
+            quantidade = quantidade - this.escudo;
+        } else {
+            quantidade = 0;
+        }
+        super.tomarDano(quantidade);
     }
 }
 
@@ -96,16 +112,12 @@ class Mago extends Personagem {
     }
 }
 
-let personagem1 = new Guerreiro("Arthur", 10, 12, 100, 1, true);
-let personagem2 = new Mago("Gendalf", 2, 8, 85, 1);
-
-console.log(personagem1);
-console.log(personagem2);
+let personagem1 = new Guerreiro("Aragorn", 10, 12, 100, 1, true, 5);
+let personagem2 = new Mago("Gendalf", 12, 8, 85, 1);
 
 console.log(personagem1.atacar(personagem2));
 console.log(personagem2.atacar(personagem1));
-console.log(personagem1.recuperarVida(15));
-console.log(personagem1.recuperarVida(15, personagem2));
+
 
 //const guerreiro = new Personagem("Arthur", 10, 12, 1); Agrega valores ao constructor.
 
