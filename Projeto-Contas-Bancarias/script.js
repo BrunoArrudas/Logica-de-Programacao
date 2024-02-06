88ed1ce7f99fede9da8d6d23a67726919c81434d
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2024-02-05 22:07:49 -0300

    Projeto-Contas-Bancarias 0.2

--- Projeto-Contas-Bancarias/script.js
+++ Projeto-Contas-Bancarias/script.js
@@ -1,49 +1,99 @@
-class Cliente{ 
-    constructor(nome,cpf){
-        this.nome= nome;
+class Cliente {
+    constructor(nome, cpf) {
+        this.nome = nome;
         this.cpf = cpf;
     }
 }
 
-class Conta{
-    constructor(cliente, numero , saldo){
+class Conta {
+    constructor(cliente, numero, saldo) {
         this.cliente = cliente;
         this.numero = numero;
         this.saldo = saldo;
-    }     
-
-    sacar(valorSaque){
-        return true;
     }
 
-    depositar(valorDeposito){
-        return true;
+    sacar(valorSaque) {
+        if (this.saldo >= valorSaque && valorSaque > 0) {
+            this.saldo -= valorSaque;
+            return true;
+        }
+
+        return false;
     }
 
-    transferir(valorTransferencia, cliente){
-        return true;
+    depositar(valorDeposito) {
+        if(valorDeposito > 0) {
+            this.saldo += valorDeposito;
+            return true;
+        }
+
+        return false;
+    }
+
+    transferir(valorTransferencia, conta) {
+        //se consigo sacar dessa conta
+        //posso depositar na conta destino
+        if(this.sacar(valorTransferencia)) {            
+            conta.depositar(valorTransferencia)
+            return true;
+        }
+        
+        return false;
     }
 }
 
-class ContaCorrente extends Conta{
-    constructor(cliente, numero , saldo, limiteChequeEspecial){
+class ContaCorrente extends Conta {
+    constructor(cliente, numero, saldo, limiteChequeEspecial) {
         super(cliente, numero, saldo);
-        this.limiteChequeEspecial = limiteChequeEspecial; //Esse valor fica somente na classe, pois e caracteristico dela, não indo para a classe super.
+        this.limiteChequeEspecial = limiteChequeEspecial;
     }
 
-    sacar(valorSaque){
-        super(valorSaque);
-        return true;
-    }
+    sacar(valorSaque) {
+        //Somar o valor do cheque especial ao valor do saldo.
+        //Verificar se pode sacar com base nessa soma.
+        const valorlimiteEspecial = this.saldo + this.limiteChequeEspecial;
+
+        if(valorSaque <= valorlimiteEspecial){
+            return super.sacar(valorSaque);
+        }
+        return false
+
+        }
 }
 
-class ContaPoupanca extends Conta{
-    constructor(cliente, numero, saldo, taxaRendimento){
+class ContaPoupanca extends Conta {
+    constructor(cliente, numero, saldo, taxaRendimento) {
         super(cliente, numero, saldo);
         this.taxaRendimento = taxaRendimento;
     }
 
-    aplicarRendimento(){
-
+    aplicarRendimento() {
+        this.saldo += this.saldo * this.taxaRendimento;
     }
 }
+
+let contas = [];
+let clientes = [];
+
+
+//let clienteA = new Cliente("Fulano","1234567890");
+//clientes.push(clienteA);
+
+//let clienteB = new Cliente("Beltrano","0987654321");
+//clientes.push(clienteB);
+
+//let contaX = new ContaCorrente(clienteA, 123, 100, 150);
+//contas.push(contaX);
+
+//let contaY = new ContaPoupanca(clienteB, 111, 100, 0.01);
+//contas.push(contaY);
+
+//contaY.transferir(50, contaX);
+
+//console.log("Conta Y: ", contaY);
+//console.log("Conta X: ", contaX);
+
+//let contaZ = new ContaCorrente(clienteB, 235, 0, 180);
+//contas.push(contaZ);
+
+//console.log(contaY.cliente.nome); Separando por pontos, eu consigo localizar o cliente ou seja o valor desejado.
